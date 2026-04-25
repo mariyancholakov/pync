fa27e0d4529131da80c59e4aa870f66cb700205b
mariyancholakov <m.cholakov09@gmail.com>
2026-04-25 17:56:49 +0300

    added gradle build config and wrapper

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.3.0"
+}
+
+group = "com.pync"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    intellijPlatform {
+        intellijIdeaCommunity("2025.1")
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+    compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+}
+
+sourceSets {
+    main {
+        kotlin {
+            exclude("com/pync/intellij/**")
+        }
+    }
+    test {
+        kotlin {
+            exclude("**")
+        }
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        version = project.version.toString()
+        ideaVersion {
+            sinceBuild = "251"
+        }
+    }
+    buildSearchableOptions = false
+}
